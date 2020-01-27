698d9ff1215f40d1ce79116a3208b3a9ec8024fa
Abdelrahman Ahmed <AbdelrahmanAbdelfattah@yahoo.com>
2020-01-27 23:12:11 +0200

    add register, login controller

--- src/main/java/com/abdo/social/controller/HomeController.java
+++ src/main/java/com/abdo/social/controller/HomeController.java
@@ -1,24 +1,41 @@
 package com.abdo.social.controller;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
-//	@RequestMapping(value="/")
-//	public ModelAndView test(HttpServletResponse response) throws IOException{
-//		return new ModelAndView("home");
-//	}
-	
 	@GetMapping("/")
-	public String test(HttpServletResponse response) throws IOException{
-		return "home/home";
+	public String showHome() {
+		
+		return "home";
 	}
+	
+	// add request mapping for /leaders
+
+	@GetMapping("/leaders")
+	public String showLeaders() {
+		
+		return "leaders";
+	}
+	
+	// add request mapping for /systems
+	
+	@GetMapping("/systems")
+	public String showSystems() {
+		
+		return "systems";
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
