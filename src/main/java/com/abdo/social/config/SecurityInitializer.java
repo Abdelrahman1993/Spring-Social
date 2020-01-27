8d57ded2a17ee63e37d4d973e83d0848c515e32e
Abdelrahman Ahmed <AbdelrahmanAbdelfattah@yahoo.com>
2020-01-27 22:42:36 +0200

    add security configuration

---
+++ src/main/java/com/abdo/social/config/SecurityInitializer.java
@@ -0,0 +1,8 @@
+package com.abdo.social.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
