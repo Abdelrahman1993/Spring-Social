fe0b9d1d2003fb1cee02c83cb2a36178baa68222
Abdelrahman Ahmed <AbdelrahmanAbdelfattah@yahoo.com>
2020-01-27 22:36:56 +0200

    add field match validator

--- src/main/java/com/abdo/social/validation/FieldMatch.java
+++ src/main/java/com/abdo/social/validation/FieldMatch.java
@@ -1,17 +1,33 @@
 package com.abdo.social.validation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+import com.abdo.social.validation.FieldMatchValidator;
+
+@Constraint(validatedBy = FieldMatchValidator.class)
+@Target({ ElementType.TYPE, ElementType.ANNOTATION_TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface FieldMatch {
-
-	public @interface List {
-
-		FieldMatch[] value();
-
-	}
-
+	String message() default "";
+	Class<?>[] groups() default {};
+	Class<? extends Payload>[] payload() default {};
+	
 	String first();
-
-	String second();
-
-	String message();
-
+    String second();
+    
+    @Target({ ElementType.TYPE, ElementType.ANNOTATION_TYPE })
+    @Retention(RetentionPolicy.RUNTIME)
+    @Documented
+    @interface List
+    {
+    	FieldMatch[] value();
+    }
 }
